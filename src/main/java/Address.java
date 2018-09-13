68da88ea8d312c561c5aa7a281c8b540034740cf
mike_glb <mike_glb@baizhi.com>
2018-09-13 21:26:06 +0800

    add 地址类

---
+++ src/main/java/Address.java
@@ -0,0 +1,33 @@
+/**
+ * @author miao
+ * @create 2018-09-13 21:24
+ */
+public class Address {
+    private int id;
+    private String city;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
